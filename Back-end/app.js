49a502159aec7b7dc6d693b281f794b68bb30942
Silverliee <44171836+Silverliee@users.noreply.github.com>
2022-02-05 16:18:20 +0100

    first commit

---
+++ Back-end/app.js
@@ -0,0 +1,33 @@
+// Appel des services (npm module tu connais )
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+//Mongoose initialisation
+mongoose.connect('mongodb+srv://mohamed:414498200@silver-cluster.yegdt.mongodb.net/HiMate?retryWrites=true&w=majority')
+	.then(() => console.log('Connexion à mon cluster mongodb réussie !'))
+	.catch(() => console.log('la connexion au cluster à échouée :('));
+
+//bodyparser access config etc
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PATCH, DELETE, OPTIONS"
+	);
+	next();
+});
+
+// gestion des controllers
+
+
+//export
+module.exports = app;
